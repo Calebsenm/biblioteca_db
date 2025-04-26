509964bdb2cf45be4086bee2a1c9614966a09404
calebsenm <calebsenm@gmail.com>
2025-04-26 18:38:46 -0500

    agregango migraciones

---
+++ server/cmd/migrate/migrations/000002_init_procedure.up.sql
@@ -0,0 +1,24 @@
+-- Procedure.
+
+CREATE PROCEDURE realizarPrestamo(
+    IN p_loanID INT,          
+    IN p_usuarioID INT,
+    IN p_libroID INT,
+    IN p_fechaPrestamo DATE,
+    IN p_fechaDevolucion DATE
+)
+BEGIN
+    DECLARE estadoLibro ENUM('disponible', 'prestado', 'reservado');
+    
+    SELECT estado INTO estadoLibro FROM libro WHERE idlibro = p_libroID;
+    
+    IF estadoLibro != 'disponible' THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El libro no está disponible para préstamo';
+    END IF;
+
+   INSERT INTO prestamo (idprestamo, idsocio, idlibro, fechaprestamo, fechadevolucion, estado)
+    VALUES (p_loanID, p_usuarioID, p_libroID, p_fechaPrestamo, p_fechaDevolucion, 'activo');
+    
+  UPDATE libro SET estado = 'prestado' WHERE idlibro = p_libroID;
+    
+END ;
--- server/go.mod
+++ server/go.mod
@@ -1,11 +1,19 @@
 module biblioteca
 
-go 1.22.0
+go 1.23.0
+
+toolchain go1.23.3
 
 require (
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/justinas/alice v1.2.0
 	github.com/rs/cors v1.11.1
 )
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
