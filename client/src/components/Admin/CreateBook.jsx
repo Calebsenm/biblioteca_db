061d2b8a1a32f61082b802cea885b2b0d7b06d3e
calebsenm <calebsenm@gmail.com>
2025-05-01 11:38:36 -0500

    fix style create book

--- client/src/components/Admin/CreateBook.jsx
+++ client/src/components/Admin/CreateBook.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './CreateBook.css';
+import '../../styles/CreateBook.css';
 
 function CreateBook() {
   const [formData, setFormData] = useState({
